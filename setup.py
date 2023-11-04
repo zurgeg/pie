6bb7d525bfdd399fe6218970ed44e21854e0e3fd
zurgeg <46549042+zurgeg@users.noreply.github.com>
2023-11-04 15:08:06 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ print("Found", find_namespace_packages(
 
 setup(
     name='pie',
-    version='0.2.0',
+    version='0.2.1',
     package_dir = {"": "src"},
     packages=find_namespace_packages(where='src'),
     include_package_data=True,
