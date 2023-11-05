33210c5dc089a425ae3405660f249267925b6866
zurgeg <46549042+zurgeg@users.noreply.github.com>
2023-11-05 08:56:36 -0500

    om nom nom

--- src/pie/crusts/notdate/__init__.py
+++ src/pie/crusts/notdate/__init__.py
@@ -1,1 +1,8 @@
+import os as _os
+from importlib import import_module as _import_module
+submodules = _os.walk('src/pie/crusts')
+for submodule in submodules:
+    if not submodule.startswith("_"):
+        print("importing", submodule)
+        globals()[submodule] = _import_module(value)
 from . import *
