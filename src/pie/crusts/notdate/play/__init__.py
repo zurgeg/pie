3223064736f3a657bf35ed718124dedff12fe6f8
zurgeg <46549042+zurgeg@users.noreply.github.com>
2023-11-05 12:08:50 -0500

    pie: now with an actually working loader

--- src/pie/crusts/notdate/play/__init__.py
+++ src/pie/crusts/notdate/play/__init__.py
@@ -1,1 +1,7 @@
-from . import *
+import os as _os
+from importlib import import_module as _import_module
+import inspect as _inspect
+submodules = next(_os.walk(_os.path.join(_os.path.dirname(_os.path.abspath(_inspect.getfile(_inspect.currentframe()))))))[1]
+for submodule in submodules:
+    if not submodule.startswith("_"):
+        globals()[submodule] = _import_module("." + submodule, __name__)
