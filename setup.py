9a8ce0d174d62b907338d49487155c4d5eb4911e
zurgeg <46549042+zurgeg@users.noreply.github.com>
2023-11-05 12:09:56 -0500

    🎉 First actual release let's goooooo!

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='pie',
-    version='0.2.1',
+    version='1.0.0',
     package_dir = {"": "src"},
     packages=find_namespace_packages(where='src'),
     include_package_data=True,
